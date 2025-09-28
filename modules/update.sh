e4286db93b9495a8aaffc72d3def85bdf92cce0c
MartianInGreen <github@rennersh.eu>
2025-09-28 12:39:05 +0200

    Update update.sh

--- modules/update.sh
+++ modules/update.sh
@@ -4,22 +4,23 @@
 source ~/.archconfig/config.sh
 
 # Update the ArchConfig repository
-git fetch $GIT_PATH
-git pull $GIT_PATH
+cd $GIT_PATH
+git fetch 
+git pull 
 
 # Update the ArchConfig application
-cp $GIT_PATH/main.sh ~/.archconfig/main.sh
+cp $GIT_PATH/main.sh $HOME/.archconfig/main.sh
 cp -r $GIT_PATH/modules/ ~/.archconfig/modules/
 
 # Compare the config.sh file
-if [ "$(diff $GIT_PATH/config.sh ~/.archconfig/config.sh)" ]; then
+if [ "$(diff $GIT_PATH/config.sh $HOME/.archconfig/config.sh)" ]; then
     printf "${YELLOW}Config file has changed, please review the changes and update the config file...${RESET}\n"
     diff $GIT_PATH/config.sh ~/.archconfig/config.sh
     printf "${YELLOW}Do you want to update the config file? ${RED}THIS WILL OVERWRITE THE CURRENT CONFIG FILE${RESET} (y/n)${RESET}\n"
     read -n 1 -r update_config
     update_config=$(echo "$update_config" | tr -d '\r')
     if [ "$update_config" == "y" ]; then
-        cp $GIT_PATH/config.sh ~/.archconfig/config.sh
+        cp $GIT_PATH/config.sh $HOME/.archconfig/config.sh
     fi
 else
     printf "${GREEN}Config file is up to date${RESET}\n"
